23728fa368b0591d6ac716416c3c144e32b9b1b5
Kamil Jakóbczak <k_j1@vp.pl>
2026-02-04 13:33:37 +0100

    add starting layout

---
+++ packages/main-site/src/App.tsx
@@ -0,0 +1,67 @@
+export default function App() {
+	return (
+		<>
+			<header>
+				<div>
+					<p>
+						Kamil <span>"Jamar"</span>Jakóbczak
+					</p>
+				</div>
+				<nav>
+					<ul>
+						<li>home</li>
+						<li>projects</li>
+						<li>3D printing</li>
+						<li>contact</li>
+						<li>light/dark toggle</li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<section id='projects'>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+					soluta quam voluptate nihil vel consequuntur optio consectetur
+					incidunt iusto harum, similique, veritatis maxime perspiciatis
+					laudantium libero ipsum asperiores! Nobis aspernatur ex explicabo
+					pariatur officia alias excepturi nemo, nulla consequatur quisquam
+					consequuntur maiores est amet deserunt voluptatibus assumenda
+					animi. Consequuntur adipisci quaerat eum deserunt. Ipsa accusamus
+					consequuntur earum! Consequatur reiciendis voluptatibus iusto
+					iure esse laborum, nulla provident rerum laudantium. Quasi porro
+					ad veniam dolore molestias eveniet debitis, saepe, voluptatum
+					dolorum ipsum reprehenderit necessitatibus labore placeat
+					temporibus ullam quos iure ut quibusdam. Magni obcaecati quo in,
+					dolore quae accusantium veniam ipsa iste.
+				</section>
+				<section id='3d-printing'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+					impedit nisi laborum expedita eveniet praesentium. Dolorum sint
+					qui corrupti reprehenderit hic. Autem minus assumenda
+					exercitationem cupiditate? Quasi dolorem ea eaque illo nobis
+					adipisci quos dolorum quae dicta laboriosam natus quis, rem
+					commodi obcaecati! Ab explicabo voluptate cupiditate mollitia
+					minima quo velit fugiat animi harum. Tempore deserunt quo
+					suscipit assumenda fuga odio voluptatem voluptas cumque tempora
+					quos commodi, aliquid modi quod quam odit adipisci veritatis
+					dolor aut consequatur sunt. Quos excepturi ipsam necessitatibus
+					voluptatum. Commodi, quas ratione dignissimos, quaerat
+					aspernatur, omnis impedit dolorum unde sed tempore asperiores
+					suscipit ad sapiente voluptate.
+				</section>
+				<section id='contact'>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+					vitae incidunt eligendi alias architecto iure dolorem at
+					recusandae eaque laboriosam fuga a non, quos veritatis beatae
+					praesentium fugit minima, quas, repellat debitis soluta
+					blanditiis. Atque, ipsum modi, repellendus doloremque nulla fuga
+					eligendi voluptatem similique, ipsam quos ut nisi blanditiis
+					voluptatum?
+				</section>
+			</main>
+			<footer>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione,
+				perspiciatis?
+			</footer>
+		</>
+	);
+}
