27a5ba9f64467c55f96ec05971137d6ce78961ed
Kamil Jakóbczak <k_j1@vp.pl>
2026-04-01 13:11:33 +0200

    adjust filter and book list styles

--- packages/bookCollection/src/components/filter/Filter.tsx
+++ packages/bookCollection/src/components/filter/Filter.tsx
@@ -3,46 +3,69 @@ import Checkbox from './Checkbox';
 import styles from './Filter.module.scss';
 
 interface FilterProps {
-  handleCheckboxChange: (e: React.ChangeEvent<HTMLInputElement>, name: string) => void;
-  filterOptions: {
-    name: string;
-    data: {
-      id: string;
-      name: string;
-      namePolish?: string;
-    }[];
-  };
+	handleCheckboxChange: (
+		e: React.ChangeEvent<HTMLInputElement>,
+		name: string,
+	) => void;
+	filterOptions: {
+		name: string;
+		data: {
+			id: string;
+			name: string;
+			namePolish?: string;
+		}[];
+	};
 }
 
-const Filter: React.FC<FilterProps> = ({ handleCheckboxChange, filterOptions }) => {
-  const { t, i18n } = useTranslation();
-  const currentLanguage = i18n.language;
-  const { name, data } = filterOptions;
-  const isPublishers = name === 'publishers';
+const Filter: React.FC<FilterProps> = ({
+	handleCheckboxChange,
+	filterOptions,
+}) => {
+	const { t, i18n } = useTranslation();
+	const currentLanguage = i18n.language;
+	const { name, data } = filterOptions;
+	const isPublishers = name === 'publishers';
 
-  const translatedName = () => {
-    if (name === 'genres') {
-      return t('genres');
-    }
-    if (name === 'publishers') {
-      return t('publishers');
-    }
-  };
+	const translatedName = () => {
+		if (name === 'genres') {
+			return t('genres');
+		}
+		if (name === 'publishers') {
+			return t('publishers');
+		}
+	};
 
-  return (
-    <div className={`${styles.category}${isPublishers ? ` ${styles.publishers}` : ''}`}>
-      <legend className={styles.legend}>{translatedName()}</legend>
-      {data.map(item => {
-        return (
-          <div key={item.name} className={styles.item}>
-            <label className={styles.formControl} htmlFor={item.name}>
-              <Checkbox name={name} id={item.id} handleCheckboxChange={handleCheckboxChange} />
-              {currentLanguage === 'pl' ? (item.namePolish ? item.namePolish : item.name) : item.name}
-            </label>
-          </div>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div
+			className={`${styles.category}${
+				isPublishers ? ` ${styles.publishers}` : ''
+			}`}>
+			<legend className={styles.legend}>{translatedName()}</legend>
+			{data.map(item => {
+				return (
+					<div
+						key={item.name}
+						className={styles.item}>
+						<label
+							className={styles.formControl}
+							htmlFor={item.name}>
+							<Checkbox
+								name={name}
+								id={item.id}
+								handleCheckboxChange={handleCheckboxChange}
+							/>
+							<span>
+								{currentLanguage === 'pl'
+									? item.namePolish
+										? item.namePolish
+										: item.name
+									: item.name}
+							</span>
+						</label>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 export default Filter;
